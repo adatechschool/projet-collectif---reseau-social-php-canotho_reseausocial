6a0382b3f4e8e79d19aebce7033123f601219358
Thomassaiag <thomas.saiag@gmail.com>
2023-06-01 11:18:14 +0200

    début étape 2, gestion des liens utilisatrices

--- Niveau1/dataBaseRequest.php
+++ Niveau1/dataBaseRequest.php
@@ -1,3 +1,3 @@
 <?php
-    $mysqli = new mysqli("localhost", "root", "root", "socialnetwork","3308");
+    $mysqli = new mysqli("localhost", "root", "root", "socialnetwork");
 ?>
--- Niveau1/followers.php
+++ Niveau1/followers.php
@@ -15,16 +15,17 @@
                 <img src = "user.jpg" alt = "Portrait de l'utilisatrice"/>
                 <section>
                     <h3>Présentation</h3>
+                    <?php $userId = intval($_GET['user_id']);?>
                     <p>Sur cette page vous trouverez la liste des personnes qui
                         suivent les messages de l'utilisatrice
-                        n° <?php echo intval($_GET['user_id']) ?></p>
+                        n° <a href="wall.php?user_id=<?php echo $userId?>"><?php echo intval($_GET['user_id']) ?></a>
 
                 </section>
             </aside>
             <main class='contacts'>
                 <?php
                 // Etape 1: récupérer l'id de l'utilisateur
-                $userId = intval($_GET['user_id']);
+                
                 // Etape 2: se connecter à la base de donnée
                 include('dataBaseRequest.php');
                 // Etape 3: récupérer le nom de l'utilisateur
@@ -39,11 +40,15 @@
                 // Etape 4: à vous de jouer
                 //@todo: faire la boucle while de parcours des abonnés et mettre les bonnes valeurs ci dessous 
                 
+
                 while ($post = $lesInformations->fetch_assoc())
-                {?>
+                {
+                
+                ?>
                 <article>
                     <img src="user.jpg" alt="blason"/>
-                    <h3><?php echo $post['alias'] ?></h3>
+                    <!-- <h3><?php echo $post['alias'] ?></h3> -->
+                    <a href="wall.php?user_id=<?php echo $post['id'] ?>"><?php echo $post['alias'] ?></a>
                     <p>id:<?php echo $post['id'] ?></p>                    
                 </article> 
                 <?php }; ?>
