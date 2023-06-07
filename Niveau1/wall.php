70aae2db0218586886f272d255a09d5afdaf6307
Thomassaiag <thomas.saiag@gmail.com>
2023-06-07 11:15:28 +0200

    post from wall by user

--- Niveau1/wall.php
+++ Niveau1/wall.php
@@ -58,7 +58,7 @@
                  */
                 $laQuestionEnSql = "
                     SELECT posts.id, posts.content, posts.created, users.alias as author_name, posts.user_id,
-                    COUNT(likes.id) as like_number, GROUP_CONCAT(DISTINCT tags.label) AS taglist 
+                    COUNT(DISTINCT likes.id) as like_number, GROUP_CONCAT(DISTINCT tags.label) AS taglist 
                     FROM posts
                     JOIN users ON  users.id=posts.user_id
                     LEFT JOIN posts_tags ON posts.id = posts_tags.post_id  
@@ -115,13 +115,68 @@
                             echo "ok ça roule";
                         }
                     }
-                    ?>   
+                ?>   
 
-                    <!-- Formulaire follow -->
-                    <form  method="post">
-                        <button name="follow">S'abonner</button>
+                <!-- Formulaire follow -->
+
+                <form  method="post">
+                    <button name="follow">S'abonner</button>
+                </form>
+                <article>
+                    <h2>Poster un message</h2>
+                    <?php
+                    /**
+                     * BD
+                     */
+                    include("dataBaseRequest.php");
                     
-                    </form>
+                    $enCoursDeTraitement = isset($_POST['message']);
+                    if ($enCoursDeTraitement)
+                    {
+                        // on ne fait ce qui suit que si un formulaire a été soumis.
+                        // Etape 2: récupérer ce qu'il y a dans le formulaire @todo: c'est là que votre travaille se situe
+                        // observez le résultat de cette ligne de débug (vous l'effacerez ensuite)
+                        echo "<pre>" . print_r($_POST, 1) . "</pre>";
+                        // et complétez le code ci dessous en remplaçant les ???
+                        $authorId = $userId;
+                        $postContent = $_POST['message'];
+
+
+                        //Etape 3 : Petite sécurité
+                        // pour éviter les injection sql : https://www.w3schools.com/sql/sql_injection.asp
+                        $authorId = intval($mysqli->real_escape_string($authorId));
+                        $postContent = $mysqli->real_escape_string($postContent);
+                        //Etape 4 : construction de la requete
+                        $lInstructionSql = "INSERT INTO posts "
+                                . "(id, user_id, content, created, parent_id) "
+                                . "VALUES (NULL, "
+                                . $authorId . ", "
+                                . "'" . $postContent . "', "
+                                . "NOW(), "
+                                . "NULL);"
+                                ;
+                        echo $lInstructionSql;
+                        // Etape 5 : execution
+                        $ok = $mysqli->query($lInstructionSql);
+                        if ( ! $ok)
+                        {
+                            echo "Impossible d'ajouter le message: " . $mysqli->error;
+                        } else
+                        {
+                            echo "Message posté en tant que :" . $authorId;
+                        }
+                    }
+                    ?>                     
+                    <form action="wall.php?user_id=<?php echo $_GET['user_id'] ?>" method="post">
+                        <input type='hidden' name='???' value='achanger'>
+                        <dl>
+                            <dt><label for='message'>Message</label></dt>
+                            <dd><textarea name='message'></textarea></dd>
+                        </dl>
+                        <input type='submit'>
+                    </form>               
+                </article>
+            </main>
 
 
 
