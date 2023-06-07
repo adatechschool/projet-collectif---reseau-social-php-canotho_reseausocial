977108e13064605e15c5ed4169811154773f6bf5
Camille Hebert <camille.hebert22@gmail.com>
2023-06-07 16:31:15 +0200

    unfollow button

--- Niveau1/wall.php
+++ Niveau1/wall.php
@@ -129,9 +129,95 @@
 
                 <!-- Formulaire follow -->
 
-                <form  method="post">
-                    <button name="follow">S'abonner</button>
-                </form>
+                
+
+<!-- Vérification si on est abonné-->
+<?php
+    
+    $followed = $_GET["user_id"];
+    $following = $_SESSION['connected_id'];
+    
+    $laQuestionEnSql2 = "SELECT * FROM followers WHERE followed_user_id= $followed AND following_user_id= $following";
+    $lesInformations2 = $mysqli->query($laQuestionEnSql2);
+    $followdata = $lesInformations2->fetch_assoc();
+    
+    if (isset($followdata)){
+        echo "<pre> coucou IF" . print_r($followdata, 1) . "</pre>";
+        $_SESSION['toggleState'] = "ON";
+    }
+    else {
+        echo "vous n'êtes pas abonné.";
+        echo "<pre> coucou ELSE" . print_r($followdata, 1) . "</pre>";
+        $_SESSION['toggleState'] = "OFF";
+    }
+    ?>
+
+
+
+<!-- Gestion du switch -->
+<?php
+
+if (isset($_POST['toggle'])) {
+    $followed = $_GET["user_id"];
+    $following = $_SESSION['connected_id'];
+    
+    
+    
+    if (isset($_SESSION['toggleState']) && $_SESSION['toggleState'] === 'ON') {
+      $_SESSION['toggleState'] = 'OFF';
+      $lInstructionSql = "DELETE FROM followers WHERE followed_user_id = $followed AND following_user_id = $following;";
+      echo $lInstructionSql;
+    } else {
+        $_SESSION['toggleState'] = 'ON';
+        $lInstructionSql = "INSERT INTO followers (followed_user_id, following_user_id)
+                          SELECT $followed, $following
+                          WHERE NOT EXISTS (
+                          SELECT $followed
+                          FROM followers
+                          WHERE followed_user_id = $followed AND following_user_id = $following
+                          );";
+        echo $lInstructionSql;
+        // Execution
+    }
+    $ok = $mysqli->query($lInstructionSql);
+    if ( ! $ok)
+    {
+        echo "ça marche pas  " ;
+    } else
+    {
+        echo "ok ça roule";
+    }
+    
+    // Perform any actions or updates based on the new state
+    // For demonstration purposes, we'll simply display the new state
+    echo 'New state: ' . $_SESSION['toggleState'];
+}
+
+?>
+
+<!-- Formulaire switch -->
+<?php  
+    if ($_SESSION['connected_id'] != $_GET["user_id"])
+    {   
+        ?>
+    <form method="POST">
+        <button type="submit" name="toggle">
+            <?php echo isset($_SESSION['toggleState']) && $_SESSION['toggleState'] === 'OFF' ? 'Follow' : 'Unfollow'; ?>
+        </button>
+    </form>
+    <?php
+        } else echo "vous ne pouvez pas vous abonner à vous-même";
+        
+        ?>
+
+
+
+
+
+
+
+
+
                 <article>
                     <h2>Poster un message</h2>
                     <?php
@@ -190,27 +276,33 @@
                             
                         }
                     }
-                    ?>                     
-                    <form action="" method="post">
-                        <input type='hidden' name='???' value='achanger'>
-                        <dl>
-                            <dt><label for='message'>Message</label></dt>
-                            <dd><textarea name='message'></textarea></dd>
-                        </dl>
-                        <input type='submit'>
-                    </form>               
-                </article>
-            </main>
+
+
+            ?>                     
 
 
 
 
-                <!-- Ajout formulaire de Nosy 
-                    
-                <form action="add_message.php" method="post">
-                    <textarea name="message" placeholder="Écrivez votre message ici"></textarea>
-                    <input type="submit" value="Publier">
-                </form> -->
+
+
+
+
+
+
+
+
+<form action="" method="post">
+    <input type='hidden' name='???' value='achanger'>
+    <dl>
+        <dt><label for='message'>Message</label></dt>
+        <dd><textarea name='message'></textarea></dd>
+    </dl>
+    <input type='submit'>
+</form>               
+</article>
+<!-- </main> -->
+
+                
 
 
 
